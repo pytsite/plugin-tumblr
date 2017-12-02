b2b26fb1dc9595f55159a93b80cf6e37497d017d
Alexander Shepetko <a@shepetko.com>
2017-12-02 11:15:14 +0200

    Release 1.1

--- _eh.py
+++ _eh.py
@@ -1,7 +1,7 @@
 """PytSite Tumblr Plugin Event Handlers
 """
-from pytsite import lang as _lang, router as _router
-from plugins import settings as _settings, auth as _auth
+from pytsite import lang as _lang, router as _router, reg as _reg
+from plugins import auth as _auth
 
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
@@ -12,5 +12,5 @@ def router_dispatch():
     """'pytsite.router.dispatch' event handler.
     """
     if _auth.get_current_user().has_permission('tumblr.settings.manage'):
-        if not _settings.get('tumblr.app_key') or not _settings.get('tumblr.app_secret'):
+        if not _reg.get('tumblr.app_key') or not _reg.get('tumblr.app_secret'):
             _router.session().add_warning_message(_lang.t('tumblr@plugin_setup_required_warning'))
